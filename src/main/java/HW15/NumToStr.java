e3b4e852d1272b06615762f5606cce3cc6a474ad
Stanislav Kondrashov <dengen1606@gmail.com>
2019-11-21 13:50:11 +0200

    Homework 15

---
+++ src/main/java/HW15/NumToStr.java
@@ -0,0 +1,111 @@
+package HW15;
+
+public class NumToStr {
+
+    public static String[] numToStr(int num) {
+        int oneNum;
+        String[] stringNum = {"", "", "", "", ""};
+        String[] temp;
+        while(num > 0) {
+            temp = stringNums(num % 10);
+            for (int i = 0; i < stringNum.length; i++) {
+                stringNum[i] = temp[i] + " " + stringNum[i];
+            }
+            num /= 10;
+        }
+        return stringNum;
+    }
+
+    public static String[] stringNums(int num) {
+        if(num == 1) {
+            String[] str = {
+                    "  @@  ",
+                    " @@@  ",
+                    "@ @@  ",
+                    "  @@  ",
+                    "@@@@@@"};
+            return str;
+        } else if(num == 2) {
+            String[] str = {
+                    " @@@@ ",
+                    "@@  @@",
+                    "   @@ ",
+                    "  @@  ",
+                    "@@@@@@"};
+            return str;
+        } else if(num == 3) {
+            String[] str = {
+                    " @@@@ ",
+                    "@@  @@",
+                    "   @@ ",
+                    "@@  @@",
+                    " @@@@ "};
+            return str;
+        } else if(num == 4) {
+            String[] str = {
+                    "   @@ ",
+                    "  @ @ ",
+                    " @  @ ",
+                    "@@@@@@",
+                    "    @ "};
+            return str;
+        } else if(num == 5) {
+            String[] str = {
+                    "@@@@@@",
+                    "@@    ",
+                    "@@@@@ ",
+                    "    @@",
+                    "@@@@@ "};
+            return str;
+        } else if(num == 6) {
+            String[] str = {
+                    " @@@@ ",
+                    "@@    ",
+                    "@@@@@ ",
+                    "@@  @@",
+                    " @@@@ "};
+            return str;
+        } else if(num == 7) {
+            String[] str = {
+                    "@@@@@@",
+                    "   @@ ",
+                    "  @@  ",
+                    " @@   ",
+                    "@@    "};
+            return str;
+        } else if(num == 8) {
+            String[] str = {
+                    " @@@@ ",
+                    "@@  @@",
+                    " @@@@ ",
+                    "@@  @@",
+                    " @@@@ "};
+            return str;
+        } else if(num == 9) {
+            String[] str = {
+                    " @@@@ ",
+                    "@@  @@",
+                    " @@@@@",
+                    "    @@",
+                    " @@@@ "};
+            return str;
+        } else if(num == 0) {
+            String[] str = {
+                    " @@@@ ",
+                    "@@  @@",
+                    "@@  @@",
+                    "@@  @@",
+                    " @@@@ "};
+            return str;
+        } else {
+            String[] str = {"ERROR"};
+            return str;
+        }
+    }
+
+    public static void strPrint(String[] strArr) {
+        for (String e : strArr) {
+            System.out.println(e);
+        }
+    }
+}
